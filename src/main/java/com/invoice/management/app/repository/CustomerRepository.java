f9ef951c2712ea18132be66e68d06cd3cf49461c
Hubert Strumiński <hubert.struminski@gmail.com>
2022-05-26 19:25:46 +0200

    added Customer, Invoice, Porduct and Tax repositories

---
+++ src/main/java/com/invoice/management/app/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.invoice.management.app.repository;
+
+import com.invoice.management.app.entity.Customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+}
